bca4b135e8da2ab9cbae20254eeed20c5e65e49c
e2402400 <mira.imene.pro.68@gmail.com>
2025-06-02 09:29:13 +0200

    maj_02-06-2025

--- Exercice2_Assurancevie.py
+++ Exercice2_Assurancevie.py
@@ -20,4 +20,25 @@ n=int(input("Entrer le nombre d'années:"))
 #<---- affiche variables ---->
 print("le capital acquis avec intérêts est de", round(c(n),2), "euros au bout de", n, "ans avec des versements mensuels") #capital acquis (c0 = capital de départ)
 print("Les intérêts gagnés au taux annuel de", t, "% sont de", round(c(n)-n*m*12-c0,2), "euros.") #
-print("Sans placement avec intérêts le capital acquis serait de", round(n*m*12+c0,2),"euros") #
+print("Sans placement avec intérêts le capital acquis serait de", round(n*m*12+c0,2),"euros")
+
+
+# Exemples 1 -->
+
+# c0 = 200 euros
+# t = 3 %
+# c^n = 10 000 euros (capital avec intérêts)
+# m = ? (--> 69 euros)
+# durée = 10 ans
+# intérêts => 1565 euros
+
+# Exemples 2 -->
+
+# c0 = 200 euros
+# t = 3 %
+# c^n = 10 000 euros
+# m = 100 euros
+# durée maximum = ?
+# intérêts
+#    n = 8 => 11 246 (intérêts = 1444)
+
---
+++ Exercice3_Prêt-mensuel.py
@@ -0,0 +1,19 @@
+def c(n):
+    if n==0:
+        return c0
+    else:
+        #return ((c(n-1)+m)*(1+t/100)**(1/12))
+        return ((c(n-1)+m)*(1+t/100/12))
+
+print("Calcul du capital acquis et des ses intérets versés lorsque les intérêts sont calculés une fois par mois")
+
+
+c0=int(input("Entrer le placement de départ (c0) :"))
+m=int(input("Entrer le montant du versement mensuel:"))
+t=float(input("Entrer me taux annuel en %:"))
+n=int(input("Entrer le nombre d'années:"))
+
+print("le capital acquis avec intérêts est de", round(c(n*12),2), "euros au bout de", n, "ans avec des versements mensuels") #capital acquis (c0 = capital de départ)
+print("Les intérêts gagnés au taux annuel de", t, "% sont de", round(c(n*12)-n*m*12-c0,2), "euros.") #
+print("Sans placement avec intérêts le capital acquis serait de", round(n*m*12+c0,2),"euros")
+        
